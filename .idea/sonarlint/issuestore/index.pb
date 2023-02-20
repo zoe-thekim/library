0e88c4908dc9d9319b3d5108afc6d6134e6aa618
zoe-thekim <sist.varm@gmail.com>
2023-02-20 16:40:28 +0900

    #1: connect db & test
    hikari 통해서 mysql 과 연결
    bean 찾지 못하는 에러는 하단의 dependencies 추가해서 해결
    
    - implementation 'org.springframework.boot:spring-boot-starter-jdbc'
    - runtimeOnly 'mysql:mysql-connector-java'
    - implementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0'

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
+7
+gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
--- src/test/java/com/web/library/LibraryApplicationTests.java
+++ src/test/java/com/web/library/LibraryApplicationTests.java
@@ -1,13 +1,48 @@
 package com.web.library;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class LibraryApplicationTests {
+class BoardApplicationTests {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private SqlSessionFactory sessionFactory;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+	public void testByApplicationContext() {
+		try {
+			System.out.println("=========================");
+			System.out.println(context.getBean("sqlSessionFactory"));
+			System.out.println("=========================");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testBySqlSessionFactory() {
+		try {
+			System.out.println("=========================");
+			System.out.println(sessionFactory.toString());
+			System.out.println("=========================");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
